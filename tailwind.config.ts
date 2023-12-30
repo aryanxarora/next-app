8bb33b73b1541b1db0309f7a8d469726632b4a39
aryanxarora <aryanxarora@gmail.com>
2023-12-30 15:02:21 -0500

    Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,29 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl' : '1536px'
     },
+    fontFamily: {
+      'sans' : ['Inter', 'ui-sans-serif', 'system-ui'],
+      'head' : ['Poppins', 'ui-sans-serif', 'system-ui'],
+      'serif' : ['Playfair Display', 'ui-sans-serif', 'system-ui'],
+    },
+    fontSize: {
+      xs: '0.65rem',
+      sm: '0.75rem',
+      md: '0.85rem',
+      base: '1rem',
+      lg: '1.15rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem'    }
   },
   plugins: [],
 }
