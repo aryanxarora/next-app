b0bcf58c0f39845b3f39649fecc6163cd832d963
aryanxarora <aryanxarora@gmail.com>
2024-01-01 21:24:16 -0500

    Setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["cdn.worldvectorlogo.com", "upload.wikimedia.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
